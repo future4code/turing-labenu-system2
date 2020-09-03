ddddb15ad37b6242083e66ca12f6a4a883a42114
Hyago Ribeiro <hyagogabriel_r@hotmail.com>
2020-09-03 11:23:59 -0300

    correção tsconfig para es6 e exemplo de implementação no Teacher.ts

--- system2/src/Teacher.ts
+++ system2/src/Teacher.ts
@@ -1,12 +1,22 @@
 import {User} from "./User";
 
+export enum TEACHER_SPECIALTY {
+    REACT = "REACT",
+    REDUX = "REDUX",
+    TESTES = "TESTES", 
+    BACKEND = "BACKEND",
+    OOP = "OOP",
+    CSS = "CSS",
+    TYPESCRIPT = "TYPESCRIPT"
+}
+
 export class Teacher implements User {
     constructor(
         public id: string,
         public name: string,
         public email: string,
         public dateOfBirth: string,
-        public skills: string,
+        public skills: TEACHER_SPECIALTY[]
     ) {}
 
     public getId(): string {
@@ -26,4 +36,15 @@ export class Teacher implements User {
     }
 }
 
-const teacher: Teacher = new Teacher("1", "Soter", "soter@gmail.com", "22/10/1989", "CSS, React, Testes, Redux")
+//Codigo para implementar no index
+
+// import {TEACHER_SPECIALTY, Teacher} from './Teacher';
+
+// const teacher: Teacher = new Teacher("1", "Soter", "soter@gmail.com", "22/10/1989", [TEACHER_SPECIALTY.REACT, TEACHER_SPECIALTY.CSS, TEACHER_SPECIALTY.REDUX, TEACHER_SPECIALTY.TESTES])
+
+// console.log(`
+//     Nome: ${teacher.name}
+//     Email: ${teacher.email}
+//     Data de nascimento: ${teacher.dateOfBirth}
+//     Especialidades: ${teacher.skills}
+// `)
