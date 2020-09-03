f9ba5e7b534a289ca2f2c73d5bfdc87837232c35
Anna Fernandes <anna.cbf@gmail.com>
2020-09-03 10:39:43 -0300

    alterei user para interface

--- system2/src/User.ts
+++ system2/src/User.ts
@@ -1,26 +1,6 @@
-export abstract class USer {
-    constructor(
-        protected id: string,
-        protected name: string,
-        protected email: string,
-        protected dateOfBirth: string,
-
-    ) { }
-
-    
-    public getId(): string {
-        return this.id;
-  }
-    
-    public getName(): string {
-        return this.name;
-  }
-    
-    public getEmail(): string {
-        return this.email;
-  }
-    
-    public getDateOfBirth(): string {
-        return this.dateOfBirth;
-  }
+export interface User {
+    id: string;
+    name: string;
+    email: string;
+    dateOfBirth: string;
 }
